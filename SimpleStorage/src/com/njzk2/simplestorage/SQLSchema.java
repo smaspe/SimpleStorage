e1b2bd0e6b58175b9e7418b06af72081ab4a8b24
njzk2 <masperos+github@gmail.com>
2013-06-18 11:52:04 +0200

    Simplify and make more extensible

--- SimpleStorage/src/com/njzk2/simplestorage/SQLSchema.java
+++ SimpleStorage/src/com/njzk2/simplestorage/SQLSchema.java
@@ -1,49 +1,36 @@
 package com.njzk2.simplestorage;
 
 import java.lang.reflect.Field;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import com.njzk2.simplestorage.handler.TypeHandler;
 
-import android.provider.BaseColumns;
-
 public class SQLSchema {
-	private List<Entry<String, String>> fields = new ArrayList<Entry<String, String>>();
+	private Map<String, TypeHandler> fields = new HashMap<String, TypeHandler>();
 
 	private Class<? extends Storable> clazz;
 
 	public SQLSchema(Class<? extends Storable> clazz) {
 		this.clazz = clazz;
-		addField(BaseColumns._ID, "integer primary key autoincrement");
 		Field[] fields = SQLHelper.getFields(clazz);
 		for (Field field : fields) {
-			addField(field.getName(), TypeHandler.getHandler(field.getType())
-					.getSQLType());
+			addField(field.getName(), TypeHandler.getHandler(field.getType()));
 		}
 	}
 
-	private void addField(String fieldName, String fieldType) {
-		fields.add(new AbstractMap.SimpleImmutableEntry<String, String>(
-				fieldName, fieldType));
+	private void addField(String fieldName, TypeHandler fieldHandler) {
+		fields.put(fieldName, fieldHandler);
 	}
 
 	public String toString() {
 		StringBuilder builder = new StringBuilder("create table ");
 		builder.append(SQLHelper.getTableName(clazz));
-		builder.append(" (");
-		boolean first = true;
-		for (Entry<String, String> field : fields) {
-			if (first) {
-				first = false;
-			} else {
-				builder.append(',');
-			}
-			builder.append(field.getKey());
-			builder.append(' ');
-			builder.append(field.getValue());
+		builder.append(" (id integer primary key autoincrement");
+		for (Entry<String, TypeHandler> field : fields.entrySet()) {
+			builder.append(',').append(field.getKey());
+			builder.append(' ').append(field.getValue().getSQLType());
 		}
 		builder.append(");");
 		return builder.toString();
