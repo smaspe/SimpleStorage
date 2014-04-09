4a9fcef10cf078518b1d17677bfb8c4632a80c17
njzk2 <masperos@gmail.com>
2014-04-09 16:56:07 -0400

    Filter static fields from field list

--- SimpleStorage/src/com/njzk2/simplestorage/SQLHelper.java
+++ SimpleStorage/src/com/njzk2/simplestorage/SQLHelper.java
@@ -1,6 +1,9 @@
 package com.njzk2.simplestorage;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +12,13 @@ public class SQLHelper {
 
 	static Field[] getFields(Class<? extends Storable> clazz) {
 		if (!fields.containsKey(clazz)) {
-			fields.put(clazz, clazz.getDeclaredFields());
+			Collection<Field> fs = new ArrayList<Field>();
+			for (Field field : clazz.getDeclaredFields()) {
+				if (!Modifier.isStatic(field.getModifiers())) {
+					fs.add(field);
+				}
+			}
+			fields.put(clazz, fs.toArray(new Field[0]));
 		}
 		return fields.get(clazz);
 	}
