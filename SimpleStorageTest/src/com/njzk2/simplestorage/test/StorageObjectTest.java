df8d220fbd9bd92b4eaba09f57afac3df5935911
njzk2 <masperos+github@gmail.com>
2014-08-12 10:54:08 -0400

    Update test values

--- SimpleStorageTest/src/com/njzk2/simplestorage/test/StorageObjectTest.java
+++ SimpleStorageTest/src/com/njzk2/simplestorage/test/StorageObjectTest.java
@@ -21,9 +21,9 @@ public class StorageObjectTest extends AndroidTestCase {
 		StorageObject object = new StorageObject();
 		object.name = "Toto";
 		object.number = 42;
-		assertTrue("Already an id", object.getId() == -1);
+		assertTrue("Already an id", object.getRowId() == -1);
 		object.save(mContext);
-		assertFalse("Failed to save", object.getId() == -1);
+		assertFalse("Failed to save", object.getRowId() == -1);
 	}
 
 	public void testLoad() throws Exception {
@@ -31,10 +31,10 @@ public class StorageObjectTest extends AndroidTestCase {
 		object.name = "Toto";
 		object.number = 42;
 		object.save(mContext);
-		object = StorageObject.getById(mContext, StorageObject.class, object.getId());
+		object = StorageObject.getById(mContext, StorageObject.class, object.getRowId());
 		assertEquals("Name not equal", "Toto", object.name);
 		assertEquals("Number not equal", 42, object.number);
-		assertFalse("Failed to load", object.getId() == -1);
+		assertFalse("Failed to load", object.getRowId() == -1);
 	}
 	@Override
 	protected void tearDown() throws Exception {
